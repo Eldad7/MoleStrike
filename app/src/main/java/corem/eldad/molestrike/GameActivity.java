9b8d8e7da7477dc1c278ada6a368ac696777fb15
Eldad Corem <eldad.corem@gmail.com>
2016-12-20 08:54:27 +0200

    First Commit

---
+++ app/src/main/java/corem/eldad/molestrike/GameActivity.java
@@ -0,0 +1,21 @@
+package corem.eldad.molestrike;
+
+import android.content.Context;
+import android.util.AttributeSet;
+
+/**
+ * Created by eldadc on 12/12/2016.
+ */
+
+public class GameActivity extends android.support.constraint.ConstraintLayout {
+    Characters chars[7];
+
+    public GameActivity(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    private class Characters{
+
+    }
+
+}
