54e5957b3a2f8e360bdcc69f4244ccac6e2c8686
Eldad Corem <eldad.corem@gmail.com>
2017-01-16 09:38:58 +0200

    local-db

--- app/src/main/java/corem/eldad/molestrike/MainActivity.java
+++ app/src/main/java/corem/eldad/molestrike/MainActivity.java
@@ -1,11 +1,16 @@
 package corem.eldad.molestrike;
 
 import android.content.Intent;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.PopupMenu;
+import android.view.Menu;
 import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
+    MoleStrikeDB MSdb;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,6 +21,40 @@ public class MainActivity extends AppCompatActivity {
     public void play(View view) {
         Intent intent = new Intent(getBaseContext(), GameActivity.class);
         startActivity(intent);
+    }
+
+    /*@Override
+    protected void onResume(){
+        super.onResume();
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                getDBData();
+            }
+        }).start();
+    }*/
+
+    private void getDBData() {
+        final SQLiteDatabase db = MSdb.getWritableDatabase();
+        String[] projection = {
+                MoleStrikeDB.player.COLUMN_NAME,
+                MoleStrikeDB.player.COLUMN_MINIMUM_LEVEL,
+                MoleStrikeDB.player.COLUMN_TOP_SCORE
+        };
+        Cursor c = db.query(
+        MoleStrikeDB.player.TABLE_NAME,           // The table to query
+        projection,                               // The columns to return
+        null,                                     // The columns for the WHERE clause
+        null,                                     // The values for the WHERE clause
+        null,                                     // don't group the rows
+        null,                                     // don't filter by row groups
+        null                                      // The sort order
+        );
+        c.moveToFirst();
+    }
+
+    public void settings(View view) {
+
     }
 
     //Need to make 2 image views with Z-index with an algorithm
--- app/src/main/java/corem/eldad/molestrike/MoleStrikeDB.java
+++ app/src/main/java/corem/eldad/molestrike/MoleStrikeDB.java
@@ -1,8 +1,83 @@
 package corem.eldad.molestrike;
 
+import android.content.ContentValues;
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+import android.provider.BaseColumns;
+
 /**
  * Created by eldadc on 04/01/2017.
  */
 
-public class MoleStrikeDB {
+public class MoleStrikeDB extends SQLiteOpenHelper {
+
+    private static final String TEXT_TYPE = " TEXT";
+    private static final String NUMBER_TYPE = " INTEGER";
+    private static final String COMMA_SEP = ",";
+    private static final String SQL_CREATE_ENTRIES =
+            "CREATE TABLE " + player.TABLE_NAME + " (" + player.COLUMN_NAME + TEXT_TYPE + COMMA_SEP +
+                    player.COLUMN_EMAIL + TEXT_TYPE + COMMA_SEP +  player.COLUMN_TOP_SCORE + NUMBER_TYPE + COMMA_SEP +
+                    player.COLUMN_MINIMUM_LEVEL + NUMBER_TYPE + COMMA_SEP + " )";
+
+    private static final String SQL_DELETE_ENTRIES =
+            "DROP TABLE IF EXISTS " + player.TABLE_NAME;
+    public static final int DATABASE_VERSION = 1;
+    public static final String DATABASE_NAME = "birthdays.db";
+
+    public MoleStrikeDB(Context context) {
+        super(context, DATABASE_NAME, null, DATABASE_VERSION);
+    }
+    public void onCreate(SQLiteDatabase db) {
+        db.execSQL(SQL_CREATE_ENTRIES);
+    }
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+        // This database is only a cache for online data, so its upgrade policy is
+        // to simply to discard the data and start over
+        db.execSQL(SQL_DELETE_ENTRIES);
+        onCreate(db);
+    }
+    public void onDowngrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+        onUpgrade(db, oldVersion, newVersion);
+    }
+
+    public static class player implements BaseColumns {
+        public static final String TABLE_NAME = "player";
+        public static final String COLUMN_NAME = "fullName";
+        public static final String COLUMN_EMAIL = "email";
+        public static final String COLUMN_TOP_SCORE = "topScore";
+        public static final String COLUMN_MINIMUM_LEVEL = "minimumLevel";
+    }
+
+    public void updateTopScore(String fullName, int topScore, SQLiteDatabase db) {
+        this.getWritableDatabase();
+        ContentValues values = new ContentValues();
+        values.put(player.COLUMN_TOP_SCORE, topScore);
+        int rows = db.update(player.TABLE_NAME, values, player.COLUMN_NAME + " = ? ", new String[]{fullName});
+    }
+
+    public void updateMinimumLevel(String fullName, int minimumLevel, SQLiteDatabase db) {
+        this.getWritableDatabase();
+        ContentValues values = new ContentValues();
+        values.put(player.COLUMN_MINIMUM_LEVEL, minimumLevel);
+        int rows = db.update(player.TABLE_NAME, values, player.COLUMN_NAME + " = ? ", new String[]{fullName});
+    }
+
+    public void facebookLogin(String fullName, String email, String oldName, SQLiteDatabase db){
+        this.getWritableDatabase();
+        ContentValues values = new ContentValues();
+        values.put(player.COLUMN_NAME, fullName);
+        values.put(player.COLUMN_EMAIL, email);
+        int rows = db.update(player.TABLE_NAME, values, player.COLUMN_NAME + " = ? ", new String[]{oldName});
+    }
+
+    public void newUser(String fullName, String email, int topScore, int minimumLevel, SQLiteDatabase db){
+        this.getWritableDatabase();
+        ContentValues values = new ContentValues();
+        values.put(player.COLUMN_NAME, fullName);
+        values.put(player.COLUMN_EMAIL, email);
+        values.put(player.COLUMN_TOP_SCORE, topScore);
+        values.put(player.COLUMN_MINIMUM_LEVEL, minimumLevel);
+        long rows = db.insert(player.TABLE_NAME, null, values);
+    }
 }
