5d1bd699fde4511fb9e69db445b503404a84d4ef
Eldad Corem <eldad.corem@gmail.com>
2017-02-05 16:54:59 +0200

    Facebook Intergration - might be cancelled

---
+++ app/src/main/java/corem/eldad/molestrike/FacebookFragment.java
@@ -0,0 +1,289 @@
+package corem.eldad.molestrike;
+
+import android.app.AlertDialog;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.support.v4.app.FragmentActivity;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.Toast;
+
+import com.facebook.AccessToken;
+import com.facebook.CallbackManager;
+import com.facebook.FacebookAuthorizationException;
+import com.facebook.FacebookCallback;
+import com.facebook.FacebookException;
+import com.facebook.FacebookSdk;
+import com.facebook.Profile;
+import com.facebook.login.LoginResult;
+import com.facebook.login.widget.LoginButton;
+import com.facebook.share.Sharer;
+import com.facebook.share.widget.ShareDialog;
+
+import java.io.InputStream;
+
+import static java.security.AccessController.getContext;
+
+/**
+ * Created by eldadc on 02/02/2017.
+ */
+
+public class FacebookFragment extends FragmentActivity{
+    private LoginButton loginButton;
+    private ImageView profilePic;
+    private Button postPhotoButton, postStatusUpdateButton, getUserInterests;
+    boolean postingEnabled = false;
+    CallbackManager callbackManager;
+    private ShareDialog shareDialog;
+    private enum PendingAction {
+        NONE,
+        POST_PHOTO,
+        POST_STATUS_UPDATE
+    }
+    private static final String PERMISSION = "publish_actions";
+    private final String PENDING_ACTION_BUNDLE_KEY =
+            "com.androidtutorialpoint.LoginActivity:PendingAction";
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        FacebookSdk.sdkInitialize(getApplicationContext());
+        loginButton = (LoginButton) findViewById(R.id.loginButton);
+        callbackManager = CallbackManager.Factory.create();
+        // Callback registration
+        loginButton.registerCallback(callbackManager, new FacebookCallback<LoginResult>() {
+            @Override
+            public void onSuccess(LoginResult loginResult) {
+                Toast toast = Toast.makeText(getBaseContext(), "Logged In", Toast.LENGTH_SHORT);
+                postingEnabled = true;
+                postPhotoButton.setVisibility(View.VISIBLE);
+                postStatusUpdateButton.setVisibility(View.VISIBLE);
+                getUserInterests.setVisibility(View.VISIBLE);
+
+                toast.show();
+                handlePendingAction();
+                updateUI();
+            }
+
+            @Override
+            public void onCancel() {
+                // App code
+                if (pendingAction != PendingAction.NONE) {
+                    showAlert();
+                    pendingAction = PendingAction.NONE;
+                }
+                updateUI();
+            }
+
+            @Override
+            public void onError(FacebookException exception) {
+                if (pendingAction != PendingAction.NONE
+                        && exception instanceof FacebookAuthorizationException) {
+                    showAlert();
+                    pendingAction = PendingAction.NONE;
+                }
+                updateUI();
+
+            }
+
+            private void showAlert() {
+                new AlertDialog.Builder(getActivity())
+                        .setTitle(R.string.cancelled)
+                        .setMessage(R.string.permission_not_granted)
+                        .setPositiveButton(R.string.ok, null)
+                        .show();
+            }
+
+        });
+        shareDialog = new ShareDialog(this);
+        shareDialog.registerCallback(
+                callbackManager, shareCallback);
+
+    }
+
+    private void updateUI() {
+        boolean enableButtons = AccessToken.getCurrentAccessToken() != null;
+
+        postStatusUpdateButton.setEnabled(enableButtons || canPresentShareDialog);
+        postPhotoButton.setEnabled(enableButtons || canPresentShareDialogWithPhotos);
+
+        Profile profile = Profile.getCurrentProfile();
+        if (enableButtons && profile != null) {
+            new LoadProfileImage(profilePicImageView).execute(profile.getProfilePictureUri(200, 200).toString());
+            greeting.setText(getString(R.string.hello_user, profile.getFirstName()));
+            postingEnabled = true;
+            postPhotoButton.setVisibility(View.VISIBLE);
+            postStatusUpdateButton.setVisibility(View.VISIBLE);
+            getUserInterests.setVisibility(View.VISIBLE);
+
+        } else {
+            Bitmap icon = BitmapFactory.decodeResource(getContext().getResources(),R.drawable.user_default);
+            profilePicImageView.setImageBitmap(ImageHelper.getRoundedCornerBitmap(getContext(), icon, 200, 200, 200, false, false, false, false));
+            greeting.setText(null);
+            postingEnabled = false;
+            postPhotoButton.setVisibility(View.GONE);
+            postStatusUpdateButton.setVisibility(View.GONE);
+            getUserInterests.setVisibility(View.GONE);
+        }
+    }
+
+
+    @Override
+    public void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+
+        outState.putString(PENDING_ACTION_BUNDLE_KEY, pendingAction.name());
+    }
+
+    private void handlePendingAction() {
+        PendingAction previouslyPendingAction = pendingAction;
+        // These actions may re-set pendingAction if they are still pending, but we assume they
+        // will succeed.
+        pendingAction = PendingAction.NONE;
+
+        switch (previouslyPendingAction) {
+            case NONE:
+                break;
+            case POST_PHOTO:
+                postPhoto();
+                break;
+            case POST_STATUS_UPDATE:
+                postStatusUpdate();
+                break;
+        }
+    }
+
+    private void onClickPostStatusUpdate() {
+        performPublish(PendingAction.POST_STATUS_UPDATE, canPresentShareDialog);
+    }
+
+    private void postStatusUpdate() {
+        Profile profile = Profile.getCurrentProfile();
+        ShareLinkContent linkContent = new ShareLinkContent.Builder()
+                .setContentTitle("Integrate Facebook Login to your Android App")
+                .setContentDescription(
+                        "This app shows how to integrate Facebook Login to your Android App")
+                .setContentUrl(Uri.parse("https://www.androidtutorialpoint.com/material-design/adding-facebook-login-to-android-app/"))
+                .build();
+        if (canPresentShareDialog) {
+            shareDialog.show(linkContent);
+        } else if (profile != null && hasPublishPermission()) {
+            ShareApi.share(linkContent, shareCallback);
+        } else {
+            pendingAction = PendingAction.POST_STATUS_UPDATE;
+        }
+    }
+
+    private void onClickPostPhoto() {
+        performPublish(PendingAction.POST_PHOTO, canPresentShareDialogWithPhotos);
+    }
+
+    private void postPhoto() {
+        Bitmap image = BitmapFactory.decodeResource(this.getResources(), R.drawable.androidlogo);
+        SharePhoto sharePhoto = new SharePhoto.Builder().setBitmap(image).build();
+        ArrayList<SharePhoto> photos = new ArrayList<>();
+        photos.add(sharePhoto);
+
+        SharePhotoContent sharePhotoContent =
+                new SharePhotoContent.Builder().setPhotos(photos).build();
+        if (canPresentShareDialogWithPhotos) {
+            shareDialog.show(sharePhotoContent);
+        } else if (hasPublishPermission()) {
+            ShareApi.share(sharePhotoContent, shareCallback);
+        } else {
+            pendingAction = PendingAction.POST_PHOTO;
+            // We need to get new permissions, then complete the action when we get called back.
+            LoginManager.getInstance().logInWithPublishPermissions(
+                    this,
+                    Arrays.asList(PERMISSION));
+        }
+    }
+
+    private boolean hasPublishPermission() {
+        AccessToken accessToken = AccessToken.getCurrentAccessToken();
+        return accessToken != null && accessToken.getPermissions().contains("publish_actions");
+    }
+
+    private void performPublish(PendingAction action, boolean allowNoToken) {
+        AccessToken accessToken = AccessToken.getCurrentAccessToken();
+        if (accessToken != null || allowNoToken) {
+            pendingAction = action;
+            handlePendingAction();
+        }
+    }
+
+    /**
+     * Background Async task to load user profile picture from url
+     * */
+    private class LoadProfileImage extends AsyncTask<String, Void, Bitmap> {
+        ImageView bmImage;
+
+        public LoadProfileImage(ImageView bmImage) {
+            this.bmImage = bmImage;
+        }
+
+        protected Bitmap doInBackground(String... uri) {
+            String url = uri[0];
+            Bitmap mIcon11 = null;
+            try {
+                InputStream in = new java.net.URL(url).openStream();
+                mIcon11 = BitmapFactory.decodeStream(in);
+            } catch (Exception e) {
+                Log.e("Error", e.getMessage());
+                e.printStackTrace();
+            }
+            return mIcon11;
+        }
+
+        protected void onPostExecute(Bitmap result) {
+
+            if (result != null) {
+
+
+                Bitmap resized = Bitmap.createScaledBitmap(result,200,200, true);
+                bmImage.setImageBitmap(ImageHelper.getRoundedCornerBitmap(getContext(),resized,250,200,200, false, false, false, false));
+
+            }
+        }
+    }
+
+    private FacebookCallback<Sharer.Result> shareCallback = new FacebookCallback<Sharer.Result>() {
+        @Override
+        public void onCancel() {
+            Log.d("FacebookFragment", "Canceled");
+        }
+
+        @Override
+        public void onError(FacebookException error) {
+            Log.d("FacebookFragment", String.format("Error: %s", error.toString()));
+            String title = getString(R.string.error);
+            String alertMessage = error.getMessage();
+            showResult(title, alertMessage);
+        }
+
+        @Override
+        public void onSuccess(Sharer.Result result) {
+            Log.d("FacebookFragment", "Success!");
+            if (result.getPostId() != null) {
+                String title = getString(R.string.success);
+                String id = result.getPostId();
+                String alertMessage = getString(R.string.successfully_posted_post, id);
+                showResult(title, alertMessage);
+            }
+        }
+
+        private void showResult(String title, String alertMessage) {
+            new AlertDialog.Builder(getBaseContext())
+                    .setTitle(title)
+                    .setMessage(alertMessage)
+                    .setPositiveButton(R.string.ok, null)
+                    .show();
+        }
+    };
+
+}
